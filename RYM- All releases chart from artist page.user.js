baad14ba9578cbec23ea614095732d03f0c295b5
sercep <37669826+sercep@users.noreply.github.com>
2024-11-09 23:47:48 +0300

    Update and rename RYM- All releases chart from artist page-1.0.user.js to RYM- All releases chart from artist page.user.js
    update to version 1.1.0 (add esoteric songs chart link for artist with low (<300) ratings)

---
+++ RYM- All releases chart from artist page.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         RYM: All releases chart from artist page
+// @namespace    https://github.com/sercep/userscripts
+// @version      1.1.0
+// @description  Add link to popular songs chart and link to all releases chart from this artist sorted by popularity (including live, archival, soundtracks and scores)
+// @author       sercep
+// @match        https://rateyourmusic.com/artist/*
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var url = document.URL;
+    var artistlink = "https://rateyourmusic.com/artist/";
+    var artistname = url.replace(artistlink, '');
+    var songratings = document.querySelector(".page_artist_tracks_track_stats_scores").lastElementChild.textContent.trim().replace("k","000");
+    // console.log(songratings);
+    if (songratings>300) {
+        document.querySelector(".page_artist_section_song_guide").innerHTML = "<span class=\"highlighted\"><i class=\"fa fa-caret-down\"></i><a href=\"https://rateyourmusic.com/charts/popular/song/all-time/a:" + artistname + "/\"> Popular songs</a></span> <span>|</span> <span class=\"\"><a href=\"https://rateyourmusic.com/charts/top/song/all-time/a:" + artistname + "/\">Top songs</a></span><span>|</span><span class=\"\"><a href=\"https://rateyourmusic.com/charts/popular/album,ep,comp,single,video,unauth,mixtape,musicvideo,djmix,additional/all-time/a:" + artistname + "/incl:live,archival,soundtrack/\">All releases</a></span>";
+    }
+    else {
+        document.querySelector(".page_artist_section_song_guide").innerHTML = "<span class=\"highlighted\"><i class=\"fa fa-caret-down\"></i><a href=\"https://rateyourmusic.com/charts/popular/song/all-time/a:" + artistname + "/\"> Popular songs</a></span> <span>|</span> <span class=\"\"><a href=\"https://rateyourmusic.com/charts/esoteric/song/all-time/a:" + artistname + "/\">Top (esoteric) songs</a></span><span>|</span><span class=\"\"><a href=\"https://rateyourmusic.com/charts/popular/album,ep,comp,single,video,unauth,mixtape,musicvideo,djmix,additional/all-time/a:" + artistname + "/incl:live,archival,soundtrack/\">All releases</a></span>";
+    }
+})();
